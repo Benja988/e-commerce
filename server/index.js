4d98c9deb03cf9ee8a8865826fc914e39f258b9f
Benjamin Okumu <okumub85@gmail.com>
2025-02-24 19:06:03 +0300

    update

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser"
 
 // Utils
 import connectDB from "./config/db.js"
+import router from "./routes/userRoutes.js"
+
 
 dotenv.config()
 const port = process.env.PORT || 5001
@@ -18,6 +20,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-app.use("/api/users", userRoutes)
+app.use("/api/users", router)
 
 app.listen(port, () => console.log(`Server running on port:➡️  ${port}`))
